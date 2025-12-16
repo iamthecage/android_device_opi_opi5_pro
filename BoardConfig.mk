fb29d92e347bbc09b06889a824e156beb528ee58
Ash Willet <asheley.willet@gmail.com>
2025-12-15 20:12:50 -0600

    Remove explicit enabling of dynamic partitions.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ BOARD_KERNEL_CMDLINE := console=ttyFIQ0 no_console_suspend root=/dev/ram0 rootwa
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 PRODUCT_MANIFEST_FILES := $(DEVICE_PATH)/product_manifest.xml
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 
 # Partition sizes
 BOARD_FLASH_BLOCK_SIZE := 4096
